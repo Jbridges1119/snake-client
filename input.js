83c6d939a8106fa81c1d01e7556ca4c8f9ebc7b4
Jeff Bridges <jbridges1119@gmail.com>
2022-06-08 21:27:18 +0000

    added movement keybinds

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,10 +11,16 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
+  if (key === 'w') connection.write('Move: up');
+  if (key === 'a') connection.write('Move: left');
+  if (key === 's') connection.write('Move: down');
+  if (key === 'd') connection.write('Move: right');
   if (key === '\u0003') {
+    console.log('Exit');
     process.exit();
+    
   }
-}
+};
 
 
-module.exports = {setupInput};
+module.exports = { setupInput };
